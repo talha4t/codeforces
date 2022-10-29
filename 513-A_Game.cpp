39f8d8b2d010ec3554c1a6e534fb7938421f46c0
Talha Mahmud <mahmudtalha80@gmail.com>
2022-10-29 20:15:46 +0600

    added 1368-A n 513-A

---
+++ 513-A_Game.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n1, n2, k1, k2;
+
+    cin >> n1 >> n2 >> k1 >> k2;
+
+    if (n1 > )
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
