e51cb3de944598c02b82c20116b0bb1772680cb4
talha4t <mahmudtalha80@gmail.com>
2022-11-30 17:10:35 +0600

    A Winter Sale

---
+++ ICPC-Assiut-University-Community-Newcomers-Training/Contest-01/A-Winter-Sale.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int
+
+int32_t main() {
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    
+    double x, p;
+    cin >> x >> p;
+
+
+    printf("%.2lf\n", (p / (1 - (x / 100))));
+    
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
