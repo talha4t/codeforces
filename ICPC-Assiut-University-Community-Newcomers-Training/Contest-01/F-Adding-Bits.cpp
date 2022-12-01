5619d7cc46c7b3e71d116542872262fb6eb8b284
talha4t <mahmudtalha80@gmail.com>
2022-12-01 18:57:19 +0600

    Adding Bits

---
+++ ICPC-Assiut-University-Community-Newcomers-Training/Contest-01/F-Adding-Bits.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int a, b;
+    cin >> a >> b;
+
+    int c = a ^ b;
+    cout << c << ed;
+
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
