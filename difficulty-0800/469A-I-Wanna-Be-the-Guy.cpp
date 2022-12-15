dd165809d3b60402a461d553a1eff951b9b87438
talha4t <mahmudtalha80@gmail.com>
2022-12-15 17:41:17 +0600

    solve one problem

--- difficulty-0800/469A-I-Wanna-Be-the-Guy.cpp
+++ difficulty-0800/469A-I-Wanna-Be-the-Guy.cpp
@@ -1,27 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int a[250];
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    string s, s1, n;
+    int n;
     cin >> n;
-    cin >> s;
-    cin >> s1;
-    
-    if (s.length() > s1.length()) {
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == n[0]) {
-                cout << "I become the guy." << "\n";
-            }
-        }
+    int p;
+    cin >> p;
+
+    for (int i = 0; i < p; i++)
+        cin >> a[i];
+
+    int q;
+    cin >> q;
+
+    for (int i = p; i < p + q; i++)
+        cin >> a[i];
+    
+    sort(a, a + (p + q));
+
+    int ct = 0;
+    for (int i = 0; i < (p + q); i++) {
+        if (a[i] != a[i + 1])
+            ct++;
     }
 
+    if (ct == n)
+        cout << "I become the guy.";
+    else 
+        cout << "Oh, my keyboard!";
 
-    
-    
+
     return 0;
 }
