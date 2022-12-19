c23885616bffe5971dd58eb07d0577056ae2cd27
talha4t <mahmudtalha80@gmail.com>
2022-12-19 22:44:25 +0600

    Solve two problem

---
+++ difficulty-0800/1772B-Matrix-Rotation.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int
+
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int a, b, c, d, tmp;
+        cin >> a >> b >> c >> d;
+
+        int yos = 0;
+        for (int i = 0; i < 4; i++) {
+            if (a < b && c < d && a < c && b < d) {
+                yos = 1;
+                break;
+            }
+            tmp = a, a = c, c = d, d = b, b = tmp;
+
+        }
+
+        if (yos)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
