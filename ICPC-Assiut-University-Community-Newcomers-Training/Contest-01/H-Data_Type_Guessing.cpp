a8d0cbd609a669a7dd579b8b148e95db5e66ed94
talha4t <mahmudtalha80@gmail.com>
2022-12-31 16:06:52 +0600

    Solve one more question

--- ICPC-Assiut-University-Community-Newcomers-Training/Contest-01/H-Data_Type_Guessing.cpp
+++ ICPC-Assiut-University-Community-Newcomers-Training/Contest-01/H-Data_Type_Guessing.cpp
@@ -8,18 +8,21 @@ int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    double n, k, a;
+    ll n, k, a;
     cin >> n >> k >> a;
 
-    ll hold = (n * k) / a;
-    double res = (n * k) / a;
+    ll ans = n * k;
 
-    if (res - hold > 0)
-        cout << "double";
-    else if (hold <= 2147483647) 
-        cout << "int";
-    else if (res > 2147483647)
-        cout << "long long";
+    if (ans % a != 0)
+        cout << "double" << ed;
+    else {
+        ll x = ans / a;
+        if (2147483648 >= x and x <= 2147483648)
+            cout << "int" << ed;
+        else 
+            cout << "long long" << ed;
+    }
+
     
     return 0;
 }
