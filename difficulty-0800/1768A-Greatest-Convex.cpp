2d450fe9ef36c08d89ef9ef775cb1e5de13e81eb
talha4t <mahmudtalha80@gmail.com>
2023-01-05 22:09:28 +0600

    Solve one problem

---
+++ difficulty-0800/1768A-Greatest-Convex.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int k;
+        cin >> k;
+
+        cout << k - 1  << ed;
+    }
+    
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
