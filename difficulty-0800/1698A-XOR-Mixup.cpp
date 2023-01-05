a7390a0d403e8f811e6b96c91c150c8026d16e97
talha4t <mahmudtalha80@gmail.com>
2023-01-05 08:54:53 +0600

    Solve one problme

---
+++ difficulty-0800/1698A-XOR-Mixup.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        int x;
+        for (int i = 0; i < n; i++)
+            cin >> x;
+
+        cout << x << ed;
+    }
+    
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
