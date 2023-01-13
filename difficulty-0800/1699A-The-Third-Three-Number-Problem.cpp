e714713a3107ec252d1fa4ad90be6077e51b164f
talha4t <mahmudtalha80@gmail.com>
2023-01-13 11:29:53 +0600

    0800 rating problem

---
+++ difficulty-0800/1699A-The-Third-Three-Number-Problem.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int 
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+
+        int n;
+        cin >> n;
+        
+        if (n % 2)
+            cout << -1 << ed;
+        else
+            cout << "0 " << n / 2 << ' ' << n / 2 << ed;
+    }
+    
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
