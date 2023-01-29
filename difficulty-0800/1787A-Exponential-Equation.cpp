b7db726fd1639076d7eb232bf5db8db7478cd578
talha4t <mahmudtalha80@gmail.com>
2023-01-29 23:29:58 +0600

    solve one problem

---
+++ difficulty-0800/1787A-Exponential-Equation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int 
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        ll n; cin >> n;
+
+        if (n % 2)
+            cout << -1 << ed;
+        else
+            cout << n / 2 << ' ' << 1 << ed;
+        
+    }
+    
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
