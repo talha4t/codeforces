0cb7ba6b3936f8a781a107882f05c759abb7d149
talha4t <mahmudtalha80@gmail.com>
2023-02-17 06:21:04 +0600

    Update

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int 
+
+bool solve() {
+
+    int a, b; cin >> a >> b;
+
+    cout << a + b << ed;
+    
+    return true;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);    
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
