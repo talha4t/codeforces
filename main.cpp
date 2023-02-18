eb86098ba80b2caec72127e91397f65224da185b
talha4t <mahmudtalha80@gmail.com>
2023-02-18 09:31:47 +0600

    update

--- main.cpp
+++ main.cpp
@@ -6,21 +6,12 @@ using namespace std;
 
 bool solve() {
 
-    int a, b; cin >> a >> b;
-
-    cout << a + b << ed;
-    
     return true;
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);    
-        freopen("output.txt", "w", stdout);
-    #endif
 
     int t;
     cin >> t;
