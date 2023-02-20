da7cbcced11da207c4699c6bf1c6e0309f930ffe
talha4t <mahmudtalha80@gmail.com>
2023-02-20 11:58:50 +0600

    Bad Boy

---
+++ difficulty-0900/1537B-Bad-Boy.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int 
+
+bool solve() {
+
+    int n, m; cin >> n >> m;
+    int a, b; cin >> a >> b;
+
+    cout << 1 << ' ' << 1 << ' ' << n << ' ' << m << ed;
+    
+    return true;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
