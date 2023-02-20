d9dfa3ee437597ad30c24798aeb1151c50a48de9
talha4t <mahmudtalha80@gmail.com>
2023-02-20 12:54:19 +0600

    digits sequence

--- difficulty-1000/1177A-Digits-Sequence-(Easy-Edition).cpp
+++ difficulty-1000/1177A-Digits-Sequence-(Easy-Edition).cpp
@@ -5,7 +5,20 @@ using namespace std;
 #define ll long long int 
 
 bool solve() {
-    
+
+    string s;
+
+    for (int i = 0; i <= 10000; i++) {
+        string in = to_string(i);
+
+        s += in;
+    }
+
+    int n; cin >> n;
+
+    cout << s[n] << ed;
+
+
     return true;
 }
 
@@ -13,12 +26,12 @@ int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t;
-    cin >> t;
+    // int t;
+    // cin >> t;
 
-    while(t--) {
+    // while(t--) {
         solve();
-    }
+    // }
     
     return 0;
 }
