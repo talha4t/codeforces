a5e5997ca9b0f656d5492280f4011c597e7bbfd6
talha4t <mahmudtalha80@gmail.com>
2023-02-22 12:38:38 +0600

    Ordinary Number

---
+++ difficulty-0800/1520B-Ordinary-Numbers.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int 
+
+bool solve() {
+    
+    int n; cin >> n;
+
+    int ct = 0;
+    for (int i = 1; i <= 9; i++) {
+        ll x = 0;
+        for (int j = 1; j <= 10; j++) {
+            x = x * 10 + i;
+
+            if (x <= n)
+                ct++;
+        }
+    }
+
+    cout << ct << ed;
+
+    return true;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
