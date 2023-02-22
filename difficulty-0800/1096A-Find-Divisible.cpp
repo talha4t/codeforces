1a4e72f717fb1b9242e43b04ac0b5ef96a0b3e93
talha4t <mahmudtalha80@gmail.com>
2023-02-22 16:13:41 +0600

    Find Divisible

---
+++ difficulty-0800/1096A-Find-Divisible.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int 
+
+bool solve() {
+    
+    ll l, r; cin >> l >> r;
+
+    cout << l << ' ' << l * 2 << ed;
+
+    return true;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
