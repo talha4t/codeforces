a907b9969b8f6abd11a50f1ffa9bdb52ccd12003
talha4t <mahmudtalha80@gmail.com>
2023-03-10 21:13:39 +0600

    optimize sereja and Dima

--- difficulty-0800/381A-Sereja-and-Dima.cpp
+++ difficulty-0800/381A-Sereja-and-Dima.cpp
@@ -39,7 +39,6 @@ bool solve() {
         }
     }
 
-
     cout << s << ' ' << d << ed;
 
     return true;
@@ -60,3 +59,68 @@ int32_t main() {
 }
 
 //github.com/talha4t (Talha Mahmud)
+
+
+// O(n)
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #define ed '\n'
+// #define ll long long int 
+// #define mod 1000000007
+
+// bool solve() {
+    
+//     int n; cin >> n;
+
+//     vector<int> v(n);
+//     for (int i = 0; i < n; i++)
+//         cin >> v[i];
+
+//     int s = 0, d = 0, f = 1;
+//     int l = 0, r = n - 1;
+//     for (int i = 0; i < n; i++) {
+//         if (f) {
+//             if (v[l] > v[r]) {
+//                 s += v[l];
+//                 l++;
+//             }
+//             else {
+//                 s += v[r];
+//                 r--;
+//             }
+//             f = 0;
+//         }
+//         else {
+//             if (v[l] > v[r]) {
+//                 d += v[l];
+//                 l++;
+//             }
+//             else {
+//                 d += v[r];
+//                 r--;
+//             }
+//             f = 1;
+//         }
+//     }
+
+//     cout << s << ' ' << d << ed;
+//     return true;
+// }
+
+// int32_t main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     // int t;
+//     // cin >> t;
+
+//     // while(t--) {
+//         solve();
+//     // }
+    
+//     return 0;
+// }
+
+// //github.com/talha4t (Talha Mahmud)
+
