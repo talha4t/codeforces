d4114c6a2a20d682ff75b1cec64267f15ef698c7
talha4t <mahmudtalha80@gmail.com>
2023-03-12 16:40:20 +0600

    Two Arrays

---
+++ difficulty-0900/1584C-Two-Arrays.cpp
@@ -0,0 +1,51 @@
+    #include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int 
+#define mod 1000000007
+
+bool solve() {
+    
+    int n; cin >> n;
+
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    if (a == b)
+        cout << "YES" << ed;
+    else {
+        for (int i = 0; i < n; i++) {
+            if (a[i] != b[i])
+                a[i] += 1;
+        }
+
+        if (a == b)
+            cout << "YES" << ed;
+        else
+            cout << "NO" << ed;
+    }
+    return true;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
