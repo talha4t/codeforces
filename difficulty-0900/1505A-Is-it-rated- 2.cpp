f29faf55ac2edad3207c140ba74ff01656559562
talha4t <mahmudtalha80@gmail.com>
2023-03-16 21:32:05 +0600

    Is it rate

---
+++ difficulty-0900/1505A-Is-it-rated- 2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s; 
+    while(getline(cin, s)) {
+        cout << "NO" << ed;
+    }
+    
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
