839cc05968330b1e0b6f646887dedffefe75be2e
talha4t <mahmudtalha80@gmail.com>
2023-03-19 10:13:15 +0600

    1475-B

---
+++ difficulty-0900/1475B-New-Year's-Number.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int 
+
+bool solve() {
+    
+    int n; cin >> n;
+
+    int mod = n % 2020;
+    int div = n / 2020;
+
+    if (mod <= div)
+        cout << "YES" << ed;
+    else
+        cout << "NO" << ed;
+
+    return true;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+//github.com/talha4t (Talha Mahmud)
