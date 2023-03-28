cebeedf7ed06c96f4ebc1d72d17e0873b213a33f
talha4t <mahmudtalha80@gmail.com>
2023-03-28 08:50:58 +0600

    Digital Root

---
+++ difficulty-1000/1107B-Digital-root.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int 
+
+bool solve() {
+    ll k, n;
+    cin >> k >> n;
+
+    cout << 9 * (k - 1) + n << ed;
+
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+// *    author:  talha4t
+//github.com/talha4t (Talha Mahmud)
+
+
