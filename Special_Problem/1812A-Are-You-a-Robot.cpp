57b9bcf533ccc1a9e18626ecf67b1849c8643efc
talha4t <mahmudtalha80@gmail.com>
2023-04-02 19:35:34 +0600

    Solved special problem

---
+++ Special_Problem/1812A-Are-You-a-Robot.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int 
+
+bool solve() {
+
+    cout << "security" << ed;
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+        solve();
+   
+    return 0;
+}
+
+// *    author:  talha4t
+//github.com/talha4t (Talha Mahmud)
---
+++ Special_Problem/1812B-Was-it-Rated.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int 
+
+bool solve() {
+    
+    int n; cin >> n;
+
+    if (n == 15 or n == 20 or n == 21)
+        cout << "NO" << ed;
+    else
+        cout << "YES" << ed;
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    
+    return 0;
+}
+
+// *    author:  talha4t
+//github.com/talha4t (Talha Mahmud)
+
+
