7c5955727f55babcee826698bf1045afde64ead5
talha4t <mahmudtalha80@gmail.com>
2023-04-16 11:10:08 +0600

    Update on ratting 900

---
+++ difficulty-0900/1339A-Filling-Diamonds.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define ll long long int 
+
+bool solve() {
+    
+    int n; cin >> n;
+
+    cout << n << ed;
+    
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+/*
+    author:  talha4t
+    github.com/talha4t (Talha Mahmud)
+    Sunday, April 16, 2023 | 11:07:45 (UTC +06:00) Dhaka
+*/
