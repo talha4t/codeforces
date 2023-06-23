c650198ade430f87e212e3c8f6547b722981c343
talha4t <mahmudtalha80@gmail.com>
2023-06-23 18:55:19 +0600

    solve a problem

--- Unrated/1843C-Sum-in-Binary-Tree.cpp
+++ Unrated/1843C-Sum-in-Binary-Tree.cpp
@@ -1,1 +1,39 @@
-cp
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int 
+
+bool solve() {
+    
+    ll n; cin >> n;
+
+    ll ans = 0;
+    while(n > 0) {
+        ans += n;
+        n /= 2;
+    }
+
+    cout << ans << '\n';
+    
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+/*
+    author:  talha4t
+    github.com/talha4t (Talha Mahmud)
+    Wednesday, June 21, 2023 | 01:06:08 (UTC +06:00) Dhaka
+*/
