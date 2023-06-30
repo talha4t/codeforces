80a7eb8228e0e904eccc0fae7e2846a13bbbf459
talha4t <mahmudtalha80@gmail.com>
2023-06-30 21:03:41 +0600

    solve a problem

---
+++ difficulty-0800/1831A-Twin Permutations.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int 
+
+bool solve() {
+    
+    int n; cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+
+    for (auto u : v) {
+        cout << ((n + 1) - u) << ' ';
+    }
+
+    cout << '\n';
+
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+/*
+    author:  talha4t
+    github.com/talha4t (Talha Mahmud)
+    Friday, June 30, 2023 | 19:32:48 (UTC +06:00) Dhaka
+*/
